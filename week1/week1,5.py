b71a32f132d5ce1100800089ddf4fa6a62df8c5e
edgar <edmorozov.uk@gmail.com>
2025-02-24 13:37:26 +0000

    week 1.5

---
+++ week1/week1,5.py
@@ -0,0 +1,14 @@
+print("my name slimshady \n sssslim shady \n slim")
+
+print("no my name is \"eddie\" alohamora")
+
+print('''okaaaayyy... let's gooo!)
+chu chu 
+ho ho ho
+he he he
+oh oh oh''')  #Merry christmas
+
+print("we are going to look at an example for input fun")
+
+name_1=input("Name?")
+print("Hello gorgeous",name_1)
