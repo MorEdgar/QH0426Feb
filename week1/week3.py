af8347e94b0dfc005a6a65597129a4a18c71246e
edgar <edmorozov.uk@gmail.com>
2025-03-03 12:39:11 +0000

    week 3

---
+++ week1/week3.py
@@ -0,0 +1,8 @@
+name = input("name")
+fav_num = int(input("number"))
+if fav_num > 500:
+    print("abrakadabra")
+elif fav_num < 500:
+    print("nox")
+elif fav_num == 500:
+    print("leave me alone")
