e8b11943a0d942c9e48ed40c56f60b802caec378
edgar <edmorozov.uk@gmail.com>
2025-03-04 22:18:30 +0000

    multiple nested decisions

---
+++ assignment1/multiple_conditions.py
@@ -0,0 +1,7 @@
+adventure = input("enter the type of adventure")
+if adventure == ("scary") and ("short"):
+    print("Entering the dark forest")
+elif adventure == ("safe") and ("long"):
+    print("Taking the safe route")
+else:
+    print("Not sure which route to take")
