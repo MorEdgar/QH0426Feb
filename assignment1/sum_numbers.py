b52327bd525c5185779d45e6c8490dd3449b93ff
edgar <edmorozov.uk@gmail.com>
2025-03-05 20:29:08 +0000

    sum numbers

---
+++ assignment1/sum_numbers.py
@@ -0,0 +1,9 @@
+#set variables
+num = 1
+total_sum = 0
+#add numbers from 1 to 100
+while num <= 100:
+    total_sum += num
+    num += 1
+#Display the result
+print("The sum of the first 100 numbers is:", total_sum)
