040bd7d3a0d3dda1c67e3949b83812bd471b2ad3
edgar <edmorozov.uk@gmail.com>
2025-03-06 11:12:13 +0000

    count down

---
+++ Uni_studying_week3/count_down.py
@@ -0,0 +1,3 @@
+steps = int(input("how far are we from the target?"))
+for steps_remaining in range(steps,0, -1):
+    print(steps_remaining,"steps remaining")
--- assignment1/loops.py
+++ assignment1/loops.py
@@ -1,10 +1,9 @@
 #first question how many bars should be charged?
-bars_to_charge = int(input("how many bars should be charged?"))
+bars = int(input("how many bars should be charged?"))
 #set charged bars to 0
 charged_bars = 0
 #loop runs until charged bars less than bars to charge
-while charged_bars < bars_to_charge:
-    print("charging...")
+while charged_bars < bars:
     charged_bars += 1 #with each loop amount of bars increment
-    print(f"Bars charged: {charged_bars}/{bars_to_charge}") # showing charged/to charge
+    print(f"Bars charged: {"\u2588" * charged_bars}")
 print("battery fully charged")
