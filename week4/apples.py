ddabf7fcd850d844d3b6c615e3ca7937002c30b5
edgar <edmorozov.uk@gmail.com>
2025-03-13 10:19:35 +0000

    while loop

---
+++ week4/apples.py
@@ -0,0 +1,5 @@
+removed_apples =int("how many apples to remove?")
+apples = 0
+while removed_apples < apples:
+    removed_apples = -1
+    print("apple removed")
---
+++ week4/nested loops.py
@@ -0,0 +1,4 @@
+for x in range(0,4):
+    for y in range(1,5):
+        print(f"({x},{y})", end="")
+    print()
